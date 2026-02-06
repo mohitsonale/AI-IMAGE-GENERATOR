9fef458627287a2de284a191399bc8d5228de469
Mohit Sonale <mohitsonale@gmail.com>
2026-02-06 10:59:27 +0530

    AI

--- AI_IMAGE_GENERATOR/backend/config/mongodb.js
+++ AI_IMAGE_GENERATOR/backend/config/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
 
     mongoose.connection.on('connected', () => {
         console.log('MongoDB connected successfully');
-    })
+    }) 
 
     await mongoose.connect(`${process.env.MONGODB_URI}/AI_Image_Generator`)
 
